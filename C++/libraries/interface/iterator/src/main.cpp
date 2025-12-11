10d248ad33c28474b01bd51fc4d8f16941613422
Patman1O1 <patrickthomas101x@gmail.com>
2025-12-10 22:14:09 -0600

    Create iterator

---
+++ C++/libraries/interface/iterator/src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstddef>
+#include <iostream>
+
+#include "iterator/iterator.hpp"
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
---
+++ C++/libraries/interface/iterator/tests/iterator_tests.cpp
@@ -0,0 +1,8 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "iterator/iterator.hpp"
+
+TEST(dummy_suite, dummy_test) {
+    EXPECT_TRUE(true);
+}
